78e9bbb0d04fc2afe1bf11c93bec3a0638516ccb
Nelson Ramirez <nramirez@stcsolutions.com.ve>
2013-06-23 01:04:39 -0430

    fix

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -30,12 +30,10 @@
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/site/index')),
-<<<<<<< HEAD
-=======
                                 array('label'=>'Saludo', 'url'=>array('/saludo/index')),
                                 array('label'=>'Tareas', 'url'=>array('/tareas/index')),
                                 array('label'=>'Ciudades', 'url'=>array('/ciudad/index')),
->>>>>>> 1a46dfb544c793db1b0bc3622c7ce037a7e7c5ec
+
 				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 				array('label'=>'Contact', 'url'=>array('/site/contact')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
